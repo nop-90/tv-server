ce634bd127dba306c4d8b3abc2ddde1b33e04410
Martineau Evan <[email]>
2017-10-07 16:59:13 +0200

    Modify example configuration to be usable Added comments in the code

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+echo "To use this script you must assure that you have installed python3 and dvblast and that you use systemd as your init process"
 echo "Installing the script (make sure you've changed the default channel path before installing). Press enter to confirm."
 read none
 cp tv /usr/local/bin/
